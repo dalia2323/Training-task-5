1c7dbacdb35241eeb458b721791a6aa112daccbe
dalia2323 <d.k.saleem@students.ptuk.edu.ps>
2023-10-21 12:37:47 +0300

    Dark mode

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import NavBar from './component/navbar';
 import './App.css';
 import Countries from './component/countries';
@@ -7,30 +7,28 @@ import SearchBar from './component/search';
 import RegionSelector from './component/region-selector';
 
 function App() {
-const[darkMode, setDarkMode]= React.useState(false);
-function toggleDarkMode(){
-  setDarkMode((prevMode)=> !prevMode)
-}
+  const [darkMode, setDarkMode] = useState(false);
+
+  function toggleDarkMode() {
+    setDarkMode((prevMode) => !prevMode);
+  }
+
   return (
     <>
-
-    <NavBar darkMode={darkMode} toggleDarkMode={toggleDarkMode} />
-    <main class="container-fluid me-lg-5 mt-4" >
-    <section className="ms-lg-5">
-      <div className="d-flex justify-content-between filter-section ">
-      <SearchBar darkMode={darkMode} />
- <RegionSelector darkMode={darkMode}/>
-      </div>
-      </section>
-    <section className="ms-lg-5 me-lg-5  d-flex justify-content-between mt-5 card-favorite-section">
-    <Favrioute />
-    <Countries />
-    </section>
-    </main>
+      <NavBar darkMode={darkMode} toggleDarkMode={toggleDarkMode} />
+      <main className={`container-fluid me-lg-5 pt-4 ${darkMode ? 'dark-mode' : ''}`}>
+        <section className="ms-lg-5">
+          <div className="d-flex justify-content-between filter-section">
+            <SearchBar darkMode={darkMode} />
+            <RegionSelector darkMode={darkMode} />
+          </div>
+        </section>
+        <section className="ms-lg-5 me-lg-5  d-flex justify-content-between mt-5 card-favorite-section">
+          <Favrioute />
+          <Countries />
+        </section>
+      </main>
     </>
-
-    
-    
   );
 }
 
